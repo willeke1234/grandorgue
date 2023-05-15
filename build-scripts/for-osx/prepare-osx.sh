cf962c2fe43624dbfa3f8ad146aea082bf130c64
willeke1234 <97431382+willeke1234@users.noreply.github.com>
2023-05-15 15:17:08 +0200

    Update prepare-osx.sh removed 10.15
    Experiment

--- build-scripts/for-osx/prepare-osx.sh
+++ build-scripts/for-osx/prepare-osx.sh
@@ -4,6 +4,7 @@ set -e
 brew install \
   cmake \
   docbook-xsl \
+  wxwidgets \
   fftw wavpack \
   gettext \
   imagemagick \
@@ -12,11 +13,6 @@ brew install \
   yaml-cpp
 brew link gettext --force
 
-# install wx for 10.15 in order to using the GrandOrgue bundle with 10.15
-# prevent upgrading wxwidgets just afrer installing
-export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
-brew install -f --formula https://github.com/GrandOrgue/WxOsX/releases/download/3.2.0-1/90dab952e7e1c26dba572e1b19b7f97728cbbd4d5580a020aee0ff36eebe9138--wxwidgets--3.2.0.catalina.bottle.tar.gz
-
 # A workaround of https://gitlab.kitware.com/cmake/cmake/-/issues/23826
 for F in $(grep -l '(, weak)\?'  /usr/local/Cellar/cmake/*/share/cmake/Modules/GetPrerequisites.cmake); do
   echo Patching $F...
@@ -24,10 +20,3 @@ for F in $(grep -l '(, weak)\?'  /usr/local/Cellar/cmake/*/share/cmake/Modules/G
   sed -e 's/(, weak)\?/(, (weak|reexport))?/' $F >${F}.new
   mv ${F}.new $F
 done
-
-# wxwidgets has been built against libtiff.5.dylib but it is not more available
-if [ ! -f /usr/local/opt/libtiff/lib/libtiff.5.dylib ]; then
-  pushd /usr/local/opt/libtiff/lib
-  sudo ln -s `readlink libtiff.dylib` libtiff.5.dylib
-  popd
-fi
